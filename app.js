83ea623c1ad52416d105252ed56d38c4e94af38f
rakshitagarwal <rakshitag13@gmail.com>
2023-01-20 12:29:34 +0530

    error solved

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require('fs');
 const app = express();
 
-const loginRoute = require('./routes/login');
-const messageRoute = require('./routes/message')
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.get("/", (req, res) => {
+    fs.readFile('username.txt', (err, data)=>{
+        if(err){
+            console.log(err);
+            data = "No data exists";
+        }
+        res.send(
+            `${data}<form action='/' method='POST' onsubmit="document.getElementById('username').value = localStorage.getItem('username')">
+                    <input type='text' name='message' id='message'>
+                    <input type="hidden" name='username' id='username'><br/>
+                    <button type='submit'>Send</button>
+                </form>`
+          );
+    })  
+});
 
-app.use(loginRoute);
+app.post("/", (req, res) => {
+    console.log(req.body.username);
+    console.log(req.body.message);
+    fs.writeFile("username.txt", `${req.body.username}:${req.body.message}`,{flag: 'a'}, (err) => 
+        err ? console.log(err) : res.redirect("/")
+    );
+});
 
-app.use(messageRoute);
+app.get("/login", (req, res) => {
+    res.send(
+    `<form action="/login" method="POST" onsubmit="localStorage.setItem('username', document.getElementById('username').value)" >
+	<input  type="text" name"username" placeHolder="username" id="username">
+	<button type="submit">add</button>
 
-app.listen(4000);
+</form>`);
+});
+
+app.post('/login',(req,res) => {
+    res.redirect('/');
+})
+
+app.listen(7000);
